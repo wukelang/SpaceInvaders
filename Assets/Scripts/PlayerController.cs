1c8ffeb8827f569293d1def2b2222cdd1b5404df
wukelang <lukey1321@gmail.com>
2025-12-26 16:08:57 +1300

    Add player X axis movement, clamping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,50 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private float moveSpeed = 10f;
+    private float minX;
+    private float maxX;
+    private float spriteWidth;
+
     void Start()
     {
-        
+        Camera cam = Camera.main;
+        float screenHalfWidth = cam.aspect * cam.orthographicSize;
+        // Debug.Log("Screen half width: " + screenHalfWidth);
+
+        SpriteRenderer spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        spriteWidth = spriteRenderer.bounds.extents.x;
+        // Debug.Log("Sprite width: " + spriteWidth);
+
+        minX = -screenHalfWidth + spriteWidth;
+        maxX = screenHalfWidth - spriteWidth;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        MovePlayer();
     }
+
+    void MovePlayer()
+    {
+        Vector2 currentPos = transform.position;
+
+        bool goLeft = Keyboard.current.leftArrowKey.isPressed || Keyboard.current.aKey.isPressed;
+        bool goRight = Keyboard.current.rightArrowKey.isPressed || Keyboard.current.dKey.isPressed;
+        if (goLeft && !goRight)
+        {
+            currentPos.x -= moveSpeed * Time.deltaTime;
+        }
+        else if (goRight && !goLeft)
+        {
+            currentPos.x += moveSpeed * Time.deltaTime;
+        }
+        currentPos.x = Mathf.Clamp(currentPos.x, minX, maxX);
+
+        transform.position = currentPos;
+    }
+
+
 }
